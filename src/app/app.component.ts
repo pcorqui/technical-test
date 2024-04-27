a80a957176435e9d53713f81b201b630c135541f
pcorqui <pcorqui@gmail.com>
2024-04-26 18:32:29 -0600

    ADD: getVersion method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { GreetingsService } from './service/greetings.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'gapsi';
 
-  version = 1.0;
+  version: string = "1.0";
 
-  constructor(private router: Router){
+  constructor(private router: Router,private greeting: GreetingsService){
 
+  }
+
+  ngOnInit(){
+
+    this.getVersion();
   }
 
   creation() {
     // Redirige al componente de creación
     this.router.navigate(['/crear']); // Ajusta la ruta según tu configuración
 }
+
+ getVersion(){
+    this.greeting.getVersion().subscribe(
+      (data: string) => {
+          this.version = data;
+      }
+  )
+ }
 }
